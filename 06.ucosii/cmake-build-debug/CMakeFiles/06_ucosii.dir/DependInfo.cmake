994d13149c92c67104494a1451c200eadee96908
huihui <503044120@qq.com>
2017-10-15 21:51:12 +0800

    sync

---
+++ 06.ucosii/cmake-build-debug/CMakeFiles/06_ucosii.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/code/cpp_tutorials/06.ucosii/CORE/ucos_ii.c" "E:/code/cpp_tutorials/06.ucosii/cmake-build-debug/CMakeFiles/06_ucosii.dir/CORE/ucos_ii.c.obj"
+  "E:/code/cpp_tutorials/06.ucosii/PORT/os_cpu_c.c" "E:/code/cpp_tutorials/06.ucosii/cmake-build-debug/CMakeFiles/06_ucosii.dir/PORT/os_cpu_c.c.obj"
+  "E:/code/cpp_tutorials/06.ucosii/PORT/os_trace.c" "E:/code/cpp_tutorials/06.ucosii/cmake-build-debug/CMakeFiles/06_ucosii.dir/PORT/os_trace.c.obj"
+  "E:/code/cpp_tutorials/06.ucosii/test.c" "E:/code/cpp_tutorials/06.ucosii/cmake-build-debug/CMakeFiles/06_ucosii.dir/test.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../CONFIG"
+  "../CORE"
+  "../PORT"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 06.ucosii/cmake-build-debug/CMakeFiles/06_ucosii.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/06_ucosii.dir/test.c.obj"
+  "CMakeFiles/06_ucosii.dir/CORE/ucos_ii.c.obj"
+  "CMakeFiles/06_ucosii.dir/PORT/os_cpu_c.c.obj"
+  "CMakeFiles/06_ucosii.dir/PORT/os_trace.c.obj"
+  "06_ucosii.pdb"
+  "06_ucosii.exe"
+  "06_ucosii.exe.manifest"
+  "lib06_ucosii.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/06_ucosii.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
